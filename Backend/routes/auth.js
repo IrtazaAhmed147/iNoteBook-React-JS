eee19b365268bc6c93212b24f0bfa63e25600bc5
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-09-11 22:18:12 +0500

    data validation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
 
 
-router.post('/', (req, res)=>{
-   console.log(req.body)
-   const user = User(req.body)
-   user.save()
-   res.send(req.body)
+router.post('/',[
+   body('name', 'enter a name').isLength({min: 3}),
+   body('email', 'enter a valid email').isEmail(),
+   body('password').isLength({min: 5}),
+
+], (req, res)=>{
+   // console.log(req.body)
+   // const user = User(req.body)
+   // user.save()
+
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array() })
+   }
+   User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    }).then(user => res.json(user));
+
+   // res.send(req.body)
 })
 
 module.exports = router
