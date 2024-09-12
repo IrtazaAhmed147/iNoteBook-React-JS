1b02083fa5f7a2bd5822bc9582d00de19e11ec61
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-09-12 19:12:21 +0500

    creating login end point & sending auth token

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -49,16 +49,66 @@ router.post('/createuser', [
          }
       }
       const authToken = jwt.sign(data, JWT_SECRET);
-      console.log(authToken)
 
+      res.json({ authToken })
 
       
-      res.json({ authToken })
    } catch (error) {
       console.log(error.message)
-      res.status(500).send("some error occur")
+      res.status(500).send("Internal Server error");
    }
 
 })
+
+
+// authenticate a user using POST "/api/auth/login", No login required
+
+router.post('/login', [
+ 
+   body('email', 'enter a valid email').isEmail(),
+   body('password', 'password cannot be blank').exists(),
+
+], async (req, res) => {
+
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+   }
+
+   const {email, password} = req.body
+   try{
+      let user = await User.findOne({email})
+      if(!user) {
+         return res.status(400).json({error: "Please try to login with correct credentials"})
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password)
+      if (!passwordCompare) {
+         return res.status(400).json({error: "Please try to login with correct credentials"})
+
+      }
+
+      const data = {
+         user:{
+
+            id: user.id
+         }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken })
+
+
+   } catch (error) {
+      console.log(error.message)
+      res.status(500).send("Internal Server error");
+   }
+
+
+})
+
+
+
+
 
 module.exports = router
